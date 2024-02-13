2d07b1b876fbe5f57ae0fefec93eda88f7cc6c40
PorosenokPoroshenko <fulkach@gmail.com>
2024-02-13 11:24:18 +0300

    frontend update, pipenv configuration

---
+++ README.md
@@ -0,0 +1,6 @@
+# blog written with fastAPI, sqlalchemy and jinja
+
+### run with
+```
+python -m src/main
+``` 
--- src/config.py
+++ src/config.py
@@ -3,6 +3,7 @@ from pydantic_settings import BaseSettings
 
 class Config(BaseSettings):
     DB_URL: str = "sqlite:///../posts.db:"
-    DB_ECHO: bool = True
+    DB_ECHO: bool = False
+
 
 config = Config()
